1cd259aa008b8762d3ffb1f435b4bea952b8d5f2
muhidham17 <49118793+muhidham17@users.noreply.github.com>
2019-10-08 15:00:48 +0800

    Create try-except-else.py

---
+++ try-except-else.py
@@ -0,0 +1,17 @@
+def main():
+  print ("Program Pembagian Bilangan")
+  
+  a = float(input("Masukkan a: "))
+  b = float(input("Masukkan b: "))
+  
+  try:
+    hasil = a / b
+  except ZeroDivisionError:
+    print ("\nError: Nilai b tidak boleh nol")
+  else:
+    print ("\na : ", a)
+    print ("b : ", b)
+    print ("a / b = ", hasil)
+    
+if __name__ == "__main__":
+ main()
